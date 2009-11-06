054057ed487c684726774a2e699efb9e683b8244
gaberudy <gaberudy@f8edfe28-6c3c-11de-a5bf-a120a255f02a>
2009-11-06 19:51:47 +0000

    * Added linux to the build configuration.  * Added a test build scripts  * Added parsing of command line arguments to override or set new properites  * Handle DOS newline encoded files from linux.

---
+++ test/test.sh
@@ -0,0 +1,7 @@
+
+[hello_world]
+echo "PWD: `pwd`"
+echo "Test String: ${test_string}"
+echo "Test Number: ${test_number}"
+echo "Test Bool Select: ${test_bool_select}"
+echo "Test Cond Select: ${test_cond_select}"
